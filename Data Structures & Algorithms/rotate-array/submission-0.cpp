752aa56874b08036b03e0db7501fc1ccc3066ade
Adarsha Kiran Khadka <94151126+32-Adarsha@users.noreply.github.com>
2026-05-15 17:09:19 -0600

    Add: rotate-array - submission-0

---
+++ Data Structures & Algorithms/rotate-array/submission-0.cpp
@@ -0,0 +1,14 @@
+
+class Solution {
+public:
+  void rotate(vector<int> &nums, int k) {
+    k = k % nums.size();
+    if (k == 0) {
+      return;
+    }
+
+    std::reverse(nums.begin(), nums.end());
+    std::reverse(nums.begin(), nums.begin() + k);
+    std::reverse(nums.begin() + k, nums.end());
+  }
+};
